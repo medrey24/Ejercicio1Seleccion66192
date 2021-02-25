2bbab316c28e0b466e8266c57d5f399642e2a207
acs <acs@win10>
2021-02-24 19:51:12 -0600

    segundoCommit_actividadRA01

--- src/ejercicio1seleccion66192/Ejercicio1Seleccion66192.java
+++ src/ejercicio1seleccion66192/Ejercicio1Seleccion66192.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
  * @author acs
  */
 public class Ejercicio1Seleccion66192 {
+    
 
     public static void main(String[] args) {
         Scanner datos = new Scanner(System.in);
